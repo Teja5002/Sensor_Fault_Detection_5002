d93778bb9f9e2626d2c6cc11103da429864eac98
Teja5002 <you@example.com>
2025-10-05 23:54:28 +0530

    updated project

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -7,9 +7,10 @@ MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://bhavyateja6:<db_password>@cluster0.llotm0e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URL="mongodb+srv://bhavyateja6:bhavyateja@cluster0.llotm0e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 MODEL_FILE_NAME = "model"
 MODEL_FILE_EXTENSION = ".pkl"
 #artifact
 artifact_folder =  "artifacts"
+
